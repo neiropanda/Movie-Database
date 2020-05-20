686d6b1956200e7a21ae9c7622ecbd6856f7e983
Oleksandr <bekh.fa@gmail.com>
2020-05-20 03:49:13 +0300

    added to repo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+}
